2af18b3b0775957ecde7abe14918caf7010e3b54
NithinRaju23 <11249A172@kanchiuniv.ac.in>
2025-11-23 00:35:02 +0530

    Update Stack_Operations.c
    Added algorithm and code comments for stack operations.

--- Stack_Operations.c
+++ Stack_Operations.c
@@ -1,4 +1,27 @@
+     //STACK//
 
+//*Aim: To find the position of a given number in a sorted list using the Binary Search method efficiently.*//
+ALGORITHM:
+ PUSH Operation:
+Step 1: Check if TOP == MAX - 1.
+Step 2: If true, display "Stack Overflow".
+Step 3: Else increment TOP by 1.
+Step 4: Set stack[TOP] = item.
+Step 5: End PUSH operation.
+  POP Operation:
+Step 1: Check if TOP == -1.
+Step 2: If true, display "Stack Underflow".
+Step 3: Else set item = stack[TOP].
+Step 4: Decrement TOP by 1.
+Step 5: Return deleted item.
+  PEEK Operation:
+Step 1: Check if TOP == -1.
+Step 2: If true, display "Stack is Empty".
+Step 3: Else set item = stack[TOP].
+Step 4: Display item.
+Step 5: End PEEK operation.
+
+CODE:
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -88,3 +111,4 @@ int main()
     }
     return 0;
 }
+
